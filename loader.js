ed37b7e100fa3d93c714d58c01d00a21bd91d425
Vinita Rane <vinitarane1998@gmail.com>
2022-06-09 15:46:42 +0530

    loader

---
+++ loader.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+    // $(".loader-screen-1").fadeOut("slow");
+
+    // $(".loader-screen-2").fadeIn("slow");
+    // $(".loader-screen-2").fadeOut("slow");
+    // $(".loader-screen-3").fadeIn("slow");
+    // $(".loader-screen-3").fadeOut("slow");
+    // $(".loader-screen-4").fadeIn("slow");
+
+    setTimeout(function(){$(".loader-screen-0").fadeOut("slow")},500);
+    setTimeout(function(){$(".loader-screen-1").fadeIn("slow")},500);
+
+    setTimeout(function(){$(".loader-screen-1").fadeOut("slow")},1000);
+    setTimeout(function(){$(".loader-screen-2").fadeIn("slow")},1000);
+
+    setTimeout(function(){$(".loader-screen-2").fadeOut("slow")},2000);
+    setTimeout(function(){$(".loader-screen-3").fadeIn("slow")},2000);
+
+    setTimeout(function(){$(".loader-screen-3").fadeOut("slow")},3000);
+    setTimeout(function(){$(".loader-screen-4").fadeIn("slow")},3000);
+
+    setTimeout(function(){$(".loader-screen-4").fadeOut("slow")},4000);
+    setTimeout(function(){$(".loader-screen-5").fadeIn("slow")},4000);
+    
+    setTimeout(function(){$(".loader-screen-5").fadeOut("slow")},5000);
+    setTimeout(function(){$(".loader-screen-6").fadeIn("slow")},5000);
+
+    setTimeout(function(){$(".loader-screen-6").fadeOut("slow")},6000);
+    setTimeout(function(){$(".loader-screen-7").fadeIn("slow")},6000);
+
+    setTimeout(function(){$(".loader-screen-7").fadeOut("slow")},7000);
+    setTimeout(function(){$(".loader-screen-8").fadeIn("slow")},7000);
+    // $(".loader-screen-5").fadeIn(16000);
+    // $(".loader-screen-5").fadeOut(4000);
+    // $(".loader-screen-6").fadeIn();
+    // $(".loader-screen-6").fadeOut(4000);
+    // $(".loader-screen-7").fadeIn();
+    // $(".loader-screen-7").fadeOut(4000);
+    // $(".loader-screen-8").fadeIn();
+});
